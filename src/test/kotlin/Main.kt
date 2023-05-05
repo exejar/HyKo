773932456acd8182bc1a3d607e82693d39a4cb15
exejar <56853437+exejar@users.noreply.github.com>
2023-05-05 01:01:23 -0400

    Updated Main test

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,5 +1,13 @@
-suspend fun main() {
-    val player = getPlayerFromUUID("98a3cecc37494981b2464fc8bb6d418c", "")
+import java.io.FileInputStream
+import java.util.*
 
-    println("${player.displayName}'s Final Kill Count: ${player.stats.bedwars.finalKills}")
+suspend fun main() {
+    /* Obviously use your own API Key, this is here so that I can test without exposing my API Key */
+    val properties = Properties()
+    properties.load(FileInputStream("config.properties"))
+
+    /* Checking stats of player "exejar" */
+    val player = getPlayerFromUUID("98a3cecc37494981b2464fc8bb6d418c", properties.getProperty("apiKey"))
+
+    println("${player.displayName}'s Void Final Kills in 4v4v4v4: ${player.stats.bedwars.fourFour.voidFinalKills}")
 }
