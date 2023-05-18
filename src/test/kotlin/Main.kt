348fba46bf45e69b3cc804b5f2d40dcd678ddc75
kk <kk>
2023-05-18 12:53:30 +0200

    test(Main.kt): switched to getPlayerByUuid

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,5 +1,5 @@
 import club.maxstats.hyko.HypixelAPIException
-import club.maxstats.hyko.getPlayerFromUUID
+import club.maxstats.hyko.getPlayerByUuid
 import java.io.File
 import java.util.*
 
@@ -9,7 +9,7 @@ suspend fun main() {
 
     /* Checking stats of player "exejar" */
     try {
-        val player = getPlayerFromUUID("98a3cecc37494981b2464fc8bb6d418c", properties.getProperty("apiKey"))
+        val player = getPlayerByUuid("98a3cecc37494981b2464fc8bb6d418c", properties.getProperty("apiKey"))
 
         println("${player.displayName}'s Void Final Kills in 4v4v4v4: ${player.stats.bedwars.fourFour.voidFinalKills}")
         println("${player.displayName}'s UHC Duels Kills: ${player.stats.duels.uhc.kills}")
